35367e6c3a52b7c0300682843f999fdbe6e92017
Курганов Никита <110167907+sooriken@users.noreply.github.com>
2022-08-22 09:56:18 +0300

    Final
    Добавлены файлы сайта

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+
+             var counter = 1;
+        setInterval(function(){
+            document.getElementById(`radio` + counter).checked = true;
+            counter++;
+            if(counter>5){
+                counter = 1;
+            }
+        },5000);   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
